b159ba34bd60dab9c77b8d1fb9c6905e1bd9efbb
Hyun <hyun_7586@naver.com>
2025-05-24 20:39:35 +0900

    refactor: entity 및 dto에서 title 필드 삭제

--- schedule.sql
+++ schedule.sql
@@ -1,7 +1,7 @@
-INSERT INTO user(name, email) VALUES("user1", "user1@gmail.com");
-INSERT INTO user(name, email) VALUES("user2", "user2@gmail.com");
-INSERT INTO user(name, email) VALUES("user3", "user3@gmail.com");
+INSERT INTO user(name, email, created_at, updated_at) VALUES("user1", "user1@gmail.com", NOW(), NOW());
+INSERT INTO user(name, email, created_at, updated_at) VALUES("user2", "user2@gmail.com", NOW(), NOW());
+INSERT INTO user(name, email, created_at, updated_at) VALUES("user3", "user3@gmail.com", NOW(), NOW());
 
-INSERT INTO schedule(title, content, password, created_at, updated_at, author) VALUES("schedule1", "1", "1111", NOW(), NOW(), 1);
-INSERT INTO schedule(title, content, password, created_at, updated_at, author) VALUES("schedule2", "2", "2222", NOW(), NOW(), 2);
-INSERT INTO schedule(title, content, password, created_at, updated_at, author) VALUES("schedule3", "3", "3333", NOW(), NOW(), 3);
+INSERT INTO schedule(content, password, created_at, updated_at, author) VALUES("schedule1", "1", "1111", NOW(), NOW(), 1);
+INSERT INTO schedule(content, password, created_at, updated_at, author) VALUES("schedule2", "2", "2222", NOW(), NOW(), 2);
+INSERT INTO schedule(content, password, created_at, updated_at, author) VALUES("schedule3", "3", "3333", NOW(), NOW(), 3);
